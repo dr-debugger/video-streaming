dcce9eda2c6f4ace09479b33702aff3e0df3578e
arijit <arijitmaiti437@gmail.com>
2023-01-04 12:45:46 +0530

    back to previous

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import RecordWebCam from "./testComponents/RecordWebCam";
+// import RecordWebCam from "./testComponents/RecordWebCam";
 
 function App() {
   return (
     <div className="App">
-      <RecordWebCam />
-      {/* <video
+      {/* <RecordWebCam /> */}
+      <video
         controls
         autoPlay
         style={{ width: "400px" }}
         src="http://localhost:8080/video"
         type="video/mp4"
-      ></video> */}
+      ></video>
     </div>
   );
 }
